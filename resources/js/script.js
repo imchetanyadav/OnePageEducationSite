a0253949d64904c05babe0245ea985ce9e9d704e
Chetan Yadav <imchetanyadav@gmail.com>
2017-02-04 19:47:26 +0530

    Fixed nav scroll animation

--- resources/js/script.js
+++ resources/js/script.js
@@ -20,23 +20,19 @@ $(document).ready(function () {
         $('html, body').animate({scrollTop: $('.js-aboutus').offset().top}, 1000);
     });
     
-    
-    /*  Smooth scrolling  */
-    $(function () {
-        $('a[href*=#]:not([href=#])').click(function () {
-            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
+    /* Scroll to section on nav click */
+    function scrolltoSection(a){
+        var s = "#" + a;
+        $('html, body').animate({scrollTop: $(s).offset().top -70}, 1000);
+        if(mobilenav){
+            nav.slideToggle(200);
+            icon.addClass('fa-bars');
+            icon.removeClass('fa-times');
+        }
+    }
+    $('.main-nav li').click(function () {
+        scrolltoSection($(this).text());
     });
-
     
     /* Animations */
     $('.js-wp-1').waypoint(function (direction) {
@@ -46,14 +42,17 @@ $(document).ready(function () {
     });
     
     /*  Mobile nav  */
+    var mobilenav = false;
+    var nav = $('.js-main-nav');
+    var icon = $('.js-mobile-nav i');
     $('.js-mobile-nav').click(function() {
-        var nav = $('.js-main-nav');
-        var icon = $('.js-mobile-nav i');
         nav.slideToggle(200);
         if ( icon.hasClass('fa-bars')) {
+            mobilenav = true;
             icon.addClass('fa-times');
             icon.removeClass('fa-bars');
         } else{
+            mobilenav = false;
             icon.addClass('fa-bars');
             icon.removeClass('fa-times');
         }
